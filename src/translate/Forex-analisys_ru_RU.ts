0b569619709af8f44cb3a82c20531dd7f52f2946
Audria Jhin <ajhin@student.21-school.ru>
2023-11-05 20:57:27 +0700

    init. rebase from gitlab

---
+++ src/translate/Forex-analisys_ru_RU.ts
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Controller</name>
+    <message>
+        <location filename="../controller/controller.cc" line="132"/>
+        <location filename="../controller/controller.cc" line="142"/>
+        <location filename="../controller/controller.cc" line="152"/>
+        <source>Load data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller/controller.cc" line="132"/>
+        <location filename="../controller/controller.cc" line="142"/>
+        <location filename="../controller/controller.cc" line="152"/>
+        <source>Data (*.csv)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>View</name>
+    <message>
+        <location filename="../forms/view.ui" line="32"/>
+        <source>Forex-analisys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="106"/>
+        <source>Interpolation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="142"/>
+        <location filename="../forms/view.ui" line="643"/>
+        <source>Degree: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="266"/>
+        <source>Points beetween: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="285"/>
+        <location filename="../forms/view.ui" line="584"/>
+        <source>Clear all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="298"/>
+        <location filename="../forms/view.ui" line="627"/>
+        <source>Value from date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="317"/>
+        <location filename="../forms/view.ui" line="516"/>
+        <source>Add graph</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="355"/>
+        <location filename="../forms/view.ui" line="554"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="360"/>
+        <location filename="../forms/view.ui" line="559"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="365"/>
+        <location filename="../forms/view.ui" line="564"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="379"/>
+        <location filename="../forms/view.ui" line="608"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="392"/>
+        <location filename="../forms/view.ui" line="478"/>
+        <source>File path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="409"/>
+        <source>Cube spline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="414"/>
+        <source>Newton&apos;s polynomial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="448"/>
+        <source>Approximation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="491"/>
+        <source>Points beetwen: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="662"/>
+        <source>M: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="672"/>
+        <source>Use users weights</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="705"/>
+        <location filename="../forms/view.ui" line="768"/>
+        <source>Research</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="720"/>
+        <source>K: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="755"/>
+        <source>H: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="775"/>
+        <source>Load data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="794"/>
+        <location filename="../forms/view.ui" line="804"/>
+        <source>Import data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forms/view.ui" line="799"/>
+        <location filename="../forms/view.ui" line="809"/>
+        <source>Export data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
